84186717a0cf31a06219aee066c9485e2552d5b3
ziedmasmoudi <zied.masmoudi2001@gmail.com>
2025-03-05 01:26:52 +0100

    add multiline support to textfield component

--- app/src/main/java/com/example/thisisawkward/components/TextField.kt
+++ app/src/main/java/com/example/thisisawkward/components/TextField.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
@@ -12,12 +12,15 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.thisisawkward.ui.theme.Gray
@@ -30,7 +33,8 @@ fun TextField(
     fieldValue: MutableState<String>,
     onChange: (String) -> Unit,
     isPasswordField: Boolean = false,
-    isNumberField: Boolean = false
+    isNumberField: Boolean = false,
+    numLines: Int = 1,
     ) {
     Column (modifier = Modifier.padding(vertical = 4.dp)){
         Text(
@@ -43,7 +47,7 @@ fun TextField(
         OutlinedTextField(
             value = fieldValue.value,
             onValueChange = { newText -> onChange(newText) },
-            shape = CircleShape,
+            shape = RoundedCornerShape((45 / 2).dp),
             colors = TextFieldDefaults.outlinedTextFieldColors(
                 containerColor = Color.White.copy(alpha = 0.4f), // Light blue background
                 focusedBorderColor = Gray,
@@ -52,10 +56,25 @@ fun TextField(
             textStyle = TextStyle(color = TextFieldGray),
             modifier = Modifier
                 .fillMaxWidth()
-                .height(45.dp),
+                .height((22 + (23 * numLines)).dp),
+            singleLine = (numLines == 1),
             keyboardOptions = if (isNumberField) KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number) else KeyboardOptions.Default,
 
             visualTransformation = if (isPasswordField) PasswordVisualTransformation() else VisualTransformation.None
         )
     }
+}
+
+@Preview
+@Composable
+fun PreviewTextField() {
+    var text = rememberSaveable { mutableStateOf("") }
+    TextField(label = "label", fieldValue = text, onChange = {text.value = it})
+}
+
+@Preview
+@Composable
+fun PreviewMultilineTextField() {
+    var text = rememberSaveable { mutableStateOf("") }
+    TextField(label = "label", fieldValue = text, onChange = {text.value = it}, numLines = 5)
 }
